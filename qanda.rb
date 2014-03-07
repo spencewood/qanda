662a31271540da6a9a80a5c83fd1ec90dd8167a8
Tyler Spencewood <tspencewood@gmail.com>
2014-03-07 21:07:21 +0700

    Adding algorithm to chop up word file. Expanding tests.

--- qanda.rb
+++ qanda.rb
@@ -1,5 +1,44 @@
 class QandA
+  attr_reader :word_dict
+
+  def questions
+    @word_dict.select{ |key,value| value.length == 1 }
+      .keys
+  end
+
+  def answers
+    @word_dict.select{ |key,value| value.length == 1 }
+      .values
+      .map{ |answer| answer.pop }
+  end
+
   def load(file_name)
-    @words = File.new(file_name, "r")
+    @words_file = File.new(file_name, "r")
+    true
+  rescue
+    false
+  end
+
+  def process
+    raise "Words file not loaded" if @words_file == nil
+    arr = []
+    @word_dict = {}
+
+    @words_file.each_line do |line|
+      line.chomp!
+      orig = line.clone
+      while line.length >= 4
+        add_word(line.slice(0, 4), orig)
+        line.slice!(0)
+      end
+    end
+  end
+
+  private
+  def add_word(key, orig)
+    unless @word_dict.has_key?(key)
+      @word_dict[key] = []
+    end
+    @word_dict[key].push(orig)
   end
 end
--- spec/qanda_spec.rb
+++ spec/qanda_spec.rb
@@ -2,9 +2,28 @@ require_relative "../qanda"
 
 describe QandA do
   describe "#load" do
-    it "should be able to load the word list" do
+    it "should return true when a word list is loaded" do
       qanda = QandA.new
-      qanda.load("spec/words")
+      expect(qanda.load("spec/words")).to eq(true)
     end
+    it "should return false when passed in name is not found" do
+      qanda = QandA.new
+      expect(qanda.load("spec/foo")).to eq(false)
+    end
+  end
+
+  describe "#process" do
+    it "should throw an error if a word list is not loaded" do
+      qanda = QandA.new
+      expect {qanda.process}.to raise_error
+    end
+  end
+
+  context "with word list" do
+    qanda = QandA.new
+    qanda.load("spec/words")
+    qanda.process
+    p qanda.questions
+    p qanda.answers
   end
 end
